72865e4db3797c685724b0f4309e1bf91153de97
Ricardo Sequeira <4726579+rpsequeira@users.noreply.github.com>
2023-12-01 08:43:24 +0000

    chsarpt day1

--- csharp/day1/Program.cs
+++ csharp/day1/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using Utils;
 using System.Text.RegularExpressions;
 
-namespace adventofcode2021
+namespace adventofcode2023
 {
     class Program
     {
@@ -25,11 +25,11 @@ namespace adventofcode2021
                 var res = DoMagic(data);
                 Console.WriteLine($"Result -> {res}");
             });
-            // Utils.Utils.MeasureActionTime("Part 2", () =>
-            // {
-            //     var res = DoMagic2(data);
-            //     Console.WriteLine($"Result -> {res}");
-            // });
+            Utils.Utils.MeasureActionTime("Part 2", () =>
+            {
+                var res = DoMagic2(data);
+                Console.WriteLine($"Result -> {res}");
+            });
             Console.WriteLine("END");
         }
 
@@ -40,12 +40,54 @@ namespace adventofcode2021
 
         static long DoMagic(IEnumerable<string> input)
         {
-            return -1;
+            int total = 0;
+            Regex rx = new Regex("[0-9]");
+            foreach (var line in input)
+            {
+                string number = "";
+                foreach (var item in line)
+                {
+                    if (char.IsNumber(item)) {
+                        number += item;
+                    }
+                }
+                string finalNumber = "";
+                finalNumber += number[0];
+                finalNumber += number[number.Length-1];
+                total += Int32.Parse(finalNumber);
+            }
+            return total;
         }
 
         static long DoMagic2(IEnumerable<string> input)
         {
-            return -1;
+            int total = 0;
+            Regex rx = new Regex("[0-9]");
+            foreach (var line in input)
+            {
+                string number = "";
+                string tempLine = line;
+                tempLine = tempLine.Replace("one","one1one");
+                tempLine = tempLine.Replace("two","two2two");
+                tempLine = tempLine.Replace("three","three3three");
+                tempLine = tempLine.Replace("four","four4four");
+                tempLine = tempLine.Replace("five","five5five");
+                tempLine = tempLine.Replace("six","six6six");
+                tempLine = tempLine.Replace("seven","seven7seven");
+                tempLine = tempLine.Replace("eight","eight8eight");
+                tempLine = tempLine.Replace("nine","nine9nine");
+                foreach (var item in tempLine)
+                {
+                    if (char.IsNumber(item)) {
+                        number += item;
+                    }
+                }
+                string finalNumber = "";
+                finalNumber += number[0];
+                finalNumber += number[number.Length-1];
+                total += Int32.Parse(finalNumber);
+            }
+            return total;
         }
     }
 }
